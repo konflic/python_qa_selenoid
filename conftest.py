109421094f8cca1758c8914ee792893ce3bca355
mchirkov <konflic@mail.ru>
2020-08-24 22:40:26 +0300

    Версия 2.0 Мобильный агент

--- conftest.py
+++ conftest.py
@@ -14,7 +14,7 @@ class MyListener(AbstractEventListener):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome")
-    parser.addoption("--selenoid", action="store", default="localhost")
+    parser.addoption("--selenoid", action="store", default="127.0.0.1")
 
 
 @pytest.fixture
@@ -25,14 +25,18 @@ def browser(request):
 
     executor_url = f"http://{selenoid}:4444/wd/hub"
 
+    options = webdriver.ChromeOptions()
+    # Передача мобильного User-Agent для хрома
+    # options.add_argument('--user-agent="Mozilla/5.0 (Windows Phone 10.0; Android 4.2.1; Microsoft; Lumia 640 XL LTE) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Mobile Safari/537.36 Edge/12.10166"')
+
     caps = {"browserName": browser,
-            # "enableVnc": True,
-            # "enableVideo": True,
+            "enableVnc": True,
+            "enableVideo": True,
             # "enableLog": True,
             # "screenResolution": "1280x720",
             "name": request.node.name}
 
-    driver = webdriver.Remote(command_executor=executor_url, desired_capabilities=caps)
+    driver = webdriver.Remote(command_executor=executor_url, desired_capabilities=caps, options=options)
 
     driver = EventFiringWebDriver(driver, MyListener())
     fixture_logger.info(f"Start session {driver.session_id}")
