d9bc4b44ea4490ee3f75146275ba5d1153d92aae
konflic <konflic@mail.ru>
2020-11-08 22:34:04 +0300

    Версия 2.1

--- conftest.py
+++ conftest.py
@@ -14,31 +14,40 @@ class MyListener(AbstractEventListener):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome")
-    parser.addoption("--selenoid", action="store", default="127.0.0.1")
+    parser.addoption("--browser_version", action="store", default="")
+    parser.addoption("--executor", action="store", default="127.0.0.1")
 
 
 @pytest.fixture
 def browser(request):
     browser = request.config.getoption("--browser")
-    selenoid = request.config.getoption("--selenoid")
+    version = request.config.getoption("--browser_version")
+    executor = request.config.getoption("--executor")
+
     fixture_logger = logging.getLogger("fixture")
 
-    executor_url = f"http://{selenoid}:4444/wd/hub"
+    executor_url = f"http://{executor}:4444/wd/hub"
 
-    options = webdriver.ChromeOptions()
-    # Передача мобильного User-Agent для хрома
-    # options.add_argument('--user-agent="Mozilla/5.0 (Windows Phone 10.0; Android 4.2.1; Microsoft; Lumia 640 XL LTE) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Mobile Safari/537.36 Edge/12.10166"')
+    options = {}
+    if browser == "chrome":
+        options = webdriver.ChromeOptions()
+        # Передача мобильного User-Agent для хрома
+        # options.add_argument('--user-agent="Mozilla/5.0 (Windows Phone 10.0; Android 4.2.1; Microsoft; Lumia 640 XL LTE) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Mobile Safari/537.36 Edge/12.10166"')
 
-    caps = {"browserName": browser,
-            "enableVnc": True,
-            "enableVideo": True,
-            # "enableLog": True,
-            # "screenResolution": "1280x720",
-            "name": request.node.name}
+    caps = {
+        "browserName": "chrome",
+        "browserVersion": "50.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": False
+        }
+    }
 
-    driver = webdriver.Remote(command_executor=executor_url, desired_capabilities=caps, options=options)
+    driver = webdriver.Remote(command_executor=executor_url, desired_capabilities=caps)
+    driver.maximize_window()
 
     driver = EventFiringWebDriver(driver, MyListener())
     fixture_logger.info(f"Start session {driver.session_id}")
+
     request.addfinalizer(driver.quit)
     return driver
