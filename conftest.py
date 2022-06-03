c14d955d8cb538fd5b7d93090fbb8468a9c47034
konflic <konflic@mail.ru>
2022-06-03 21:57:24 +0300

    Версия 4.1

--- conftest.py
+++ conftest.py
@@ -1,21 +1,25 @@
 import pytest
 from selenium import webdriver
-
+from selenium.webdriver.opera.options import Options
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome")
-    parser.addoption("--executor", action="store", default="192.168.1.95")
+    parser.addoption("--executor", action="store", default="192.168.43.97")
     parser.addoption("--mobile", action="store_true")
+    parser.addoption("--vnc", action="store_true")
+    parser.addoption("--logs", action="store_true")
+    parser.addoption("--videos", action="store_true")
+    parser.addoption("--bv")
 
 
 @pytest.fixture
 def browser(request):
     browser = request.config.getoption("--browser")
     executor = request.config.getoption("--executor")
-    # version = request.config.getoption("--bversion")
-    # vnc = request.config.getoption("--vnc")
-    # logs = request.config.getoption("--logs")
-    # videos = request.config.getoption("--videos")
+    version = request.config.getoption("--bv")
+    vnc = request.config.getoption("--vnc")
+    logs = request.config.getoption("--logs")
+    videos = request.config.getoption("--videos")
     mobile = request.config.getoption("--mobile")
 
     if executor == "local":
@@ -31,26 +35,31 @@ def browser(request):
 
         caps = {
             "browserName": browser,
-            # "browserVersion": version,
+            "browserVersion": version,
             # "screenResolution": "1280x720",
-            # "name": "Mikhail",
-            # "selenoid:options": {
-            #     "enableVNC": vnc,
-            #     "enableVideo": videos,
-            #     "enableLog": logs
-            # },
-            # 'acceptSslCerts': True,
-            # 'acceptInsecureCerts': True,
-            # 'timeZone': 'Europe/Moscow',
+            "name": "Mikhail",
+            "selenoid:options": {
+                "enableVNC": vnc,
+                "enableVideo": videos,
+                "enableLog": logs
+            },
+            'acceptSslCerts': True,
+            'acceptInsecureCerts': True,
+            'timeZone': 'Europe/Moscow',
             'goog:chromeOptions': {}
         }
 
         if browser == "chrome" and mobile:
             caps["goog:chromeOptions"]["mobileEmulation"] = {"deviceName": "iPhone 5/SE"}
 
+        options = Options()
+        if browser == "opera":
+            options.add_experimental_option('w3c', True)
+
         driver = webdriver.Remote(
             command_executor=executor_url,
-            desired_capabilities=caps
+            desired_capabilities=caps,
+            options=options
         )
 
         if not mobile:
--- tests/test_opencart_local.py
+++ tests/test_opencart_local.py
@@ -1,5 +1,5 @@
 
 
 def test_open_local_url(browser):
-    browser.get("http://192.168.1.88:8081/")
+    browser.get("http://192.168.20.134 :8081/")
     assert browser.title == "Your Store"
