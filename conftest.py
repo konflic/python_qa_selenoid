c0098c2769bd0d78b64fc58f932d4cdff2404dde
konflic <konflic@mail.ru>
2024-11-11 10:52:28 +0300

    Версия 6.0 Обновление библиотек

--- conftest.py
+++ conftest.py
@@ -12,39 +12,12 @@ from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome")
-    parser.addoption("--executor", action="store", default="127.0.0.1")
+    parser.addoption("--executor", action="store", default="192.168.0.114")
     parser.addoption("--mobile", action="store_true")
     parser.addoption("--vnc", action="store_true")
     parser.addoption("--logs", action="store_true")
     parser.addoption("--video", action="store_true")
     parser.addoption("--bv")
-
-
-# @allure.step("Waiting for availability {url}")
-# def wait_url_data(url, timeout=10):
-#     """Метод ожидания доступности урла"""
-#     while timeout:
-#         response = requests.get(url)
-#         if not response.ok:
-#             time.sleep(1)
-#             timeout -= 1
-#         else:
-#             if "video" in url:
-#                 return response.content
-#             else:
-#                 return response.text
-#     return None
-
-
-# @pytest.hookimpl(tryfirst=True, hookwrapper=True)
-# # https://github.com/pytest-dev/pytest/issues/230#issuecomment-402580536
-# def pytest_runtest_makereport(item, call):
-#     outcome = yield
-#     rep = outcome.get_result()
-#     if rep.outcome != "passed":
-#         item.status = "failed"
-#     else:
-#         item.status = "passed"
 
 
 @pytest.fixture
@@ -91,19 +64,6 @@ def browser(request):
         driver.maximize_window()
 
     def finalizer():
-        # video_url = f"http://{executor}:8080/video/{driver.session_id}.mp4"
-        #
-        # if request.node.status == "failed":
-        #     if video:
-        #         allure.attach(
-        #             body=wait_url_data(video_url),
-        #             name="video_for_" + driver.session_id,
-        #             attachment_type=allure.attachment_type.MP4,
-        #         )
-        #
-        # if video and wait_url_data(video_url):
-        #     requests.delete(url=video_url)
-
         driver.quit()
 
     request.addfinalizer(finalizer)
